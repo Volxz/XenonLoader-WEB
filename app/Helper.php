9b3fccb93d8d9a45308375afd35d1cf6c2aed423
Ethan Gallant <ethan.gallant@gmail.com>
2019-03-24 17:58:30 -0400

    Have helper take in request data for simplicity in controllers/routes

--- app/Helper.php
+++ app/Helper.php
@@ -3,13 +3,16 @@
 /**
  * Format data to JSON then encrypt and B64 it then make it a JSON again (easy right?)
  * @param $data
- * @param $key
+ * @param $request
  * @return array
  */
-function formatAndEncrypt($data, $key){
-    openssl_public_encrypt(json_encode($data), $encrypted, $key);
+function formatAndEncrypt($data, $request){
+    $loader = \App\Models\Loader::where('version', '=', $request->header('loader_version'))->first();
+    $publicKey = Storage::get($loader->encryption_key_public);
+    openssl_public_encrypt(json_encode($data), $encrypted, $publicKey);
     $encryptedB64 = base64_encode($encrypted);
     $response = [];
     $response['data'] = $encryptedB64;
     return $response;
 }
+
