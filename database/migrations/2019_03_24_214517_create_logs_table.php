31126bdf56911a857da531d83680b40e2f6b9801
Ethan Gallant <ethan.gallant@gmail.com>
2019-03-24 17:59:23 -0400

    Create logs table migration

---
+++ database/migrations/2019_03_24_214517_create_logs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('xf_user_id');
+            $table->ipAddress('client_ip');
+            $table->string('client_hwid');
+            $table->string('event_type');
+            $table->boolean('success');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logs');
+    }
+}
