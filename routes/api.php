7d365b0ca1652e1739f3fc8f3826c060afd806e4
Ethan Gallant <ethan.gallant@gmail.com>
2019-03-24 17:58:52 -0400

    Big changes to API routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Middleware\EncryptedLoaderAPI;
 use App\Http\Middleware\XFAuth;
+use App\Models\Mod;
 use App\Models\XFUser;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
@@ -20,33 +21,46 @@ use Illuminate\Support\Facades\Storage;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/modinfo', function () {
-    return App\Models\Mod::with('game')->get()->toJSON();
-});
 
-Route::post('/launcher/checklogin', function (Request $request) {
-    //TODO: Add route field verification!!!! Spaces CRASH THE THREAD
-    $loader = \App\Models\Loader::where('version', '=', $request->header('loader_version'))->first();
-    $publicKey = Storage::get($loader->encryption_key_public);
+
+Route::post('/loader/checklogin', function (Request $request) {
+    //TODO: REQUIRE AND VERIFY HWID
+    $request->validate([
+        'username' => 'required',
+        'password' => 'required',
+        'token'    => 'required'
+    ]);
+
     $data['token'] = $request->get('token');
-    $user = XFUser::where('username', '=', $request->get('username'))->first();
-    if(!$user){
+    $username =  $request->get('username');
+    $user = XFUser::where('username', '=', $username)->first();
+    if(!$user) {
         $data['success'] = false;
+        $data['error'] = "User " .  $username . " was not found.";
     } else {
-        $data['success'] = $user->checkPassword($request->get('password'));
+        if ($user->checkPassword($request->get('password'))) {
+            $data['success'] = true;
+        } else {
+            $data['success'] = false;
+            $data['error'] = "The password entered was incorrect.";
+        }
     }
-    return response()->json(formatAndEncrypt($data, $publicKey));
+
+    return response()->json(formatAndEncrypt($data, $request));
 })->middleware(EncryptedLoaderAPI::class);
 
-Route::post('/test', function (Request $request) {
-    $xfUser = XFUser::where('username','=','sp3ctre')->first()->xfGroups()->get();
-         return $xfUser;
-})->middleware(EncryptedLoaderAPI::class, XFAuth::class);
 
-Route::post('/launcher/modlist', function (Request $request){
+
+Route::post('/loader/modlist', function (Request $request){
+    $request->validate([
+        'username' => 'required',
+        'password' => 'required',
+        'token'    => 'required'
+    ]);
     //Get individual mods assigned to the groups
-    $xfUser = XFUser::where('username','=',$request->get('user'))->with('xfgroups.mods.game','xfgroups.games.mods')->get()->first();
+    $xfUser = XFUser::where('username','=',$request->get('username'))->with('xfgroups.mods.game','xfgroups.games.mods')->get()->first();
     $availableMods = [];
+    $response = [];
     $groups = $xfUser->toArray()['xfgroups'];
     foreach($groups as $group)
     {
@@ -56,7 +70,8 @@ Route::post('/launcher/modlist', function (Request $request){
             $temparr['id'] = $mod['id'];
             $temparr['version'] = $mod['version'];
             $temparr['name'] = $mod['name'];
-            $temparr['game'] = $mod['game']['name'];
+            $temparr['game']['name'] = $mod['game']['name'];
+            $temparr['game']['executable'] = $mod['game']['executable'];
 
             array_push($availableMods,$temparr);
         }
@@ -68,15 +83,42 @@ Route::post('/launcher/modlist', function (Request $request){
                 $temparr['id'] = $mod['id'];
                 $temparr['version'] = $mod['version'];
                 $temparr['name'] = $mod['name'];
-                $temparr['game'] = $game['name'];
+                $temparr['game']['name'] = $game['name'];
+                $temparr['game']['executable'] = $game['executable'];
+
                 array_push($availableMods,$temparr);
             }
         }
     }
 
     //
+    $availableMods = array_values(array_map("unserialize", array_unique(array_map("serialize", $availableMods))));
+    $response['data'] = $availableMods;
+    $response['success'] = true;
+    $response['token'] = $request->get('token');
+    return response()->json($response);
+    //return response()->json(formatAndEncrypt($response, $request));
+})->middleware(EncryptedLoaderAPI::class,XFAuth::class);
 
+Route::post('/loader/downloadmod', function (Request $request) {
+    $mod = Mod::find($request)->first();
 
+    $xfUser = XFUser::where('username','=',$request->get('username'))->with('xfgroups.mods.game','xfgroups.games.mods')->get()->first();
+    $availableModsIDs = [];
+    $groups = $xfUser->toArray()['xfgroups'];
+    foreach($groups as $group) {
+        foreach ($group['mods'] as $mod) {
+            array_push($availableMods, $mod['id']);
+        }
 
-    return response()->json(array_map("unserialize", array_unique(array_map("serialize", $availableMods))));
-})->middleware(XFAuth::class); // TODO: Test in client and add encrypted channels
+        foreach ($group['games'] as $game) {
+            foreach ($game['mods'] as $mod) {
+                array_push($availableMods, $mod['id']);
+            }
+        }
+    }
+    if(!in_array($mod->id, $availableModsIDs))
+        abort(403);
+    return Response::download(storage_path("app/{$mod->mod_location}"), 'inject.dll');
+
+})->middleware(EncryptedLoaderAPI::class,XFAuth::class);
