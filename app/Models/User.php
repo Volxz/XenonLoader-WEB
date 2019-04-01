58cc37cadf8a5235bfe438c3fa8548868c5f6f7d
Ethan Gallant <ethan.gallant@gmail.com>
2019-04-01 16:40:12 -0400

    Add user manager to panel

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-
+use Backpack\CRUD\CrudTrait;
 class User extends Authenticatable
 {
     use Notifiable;
